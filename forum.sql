66c2823e21bd98f3c057c956eb26245236d78cb9
Mustapha-Mohamed <55873657+Mustapha-Mohamed@users.noreply.github.com>
2020-10-20 14:17:56 +0200

    Add files via upload

--- forum.sql
+++ forum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 12 fév. 2020 à 09:20
+-- Généré le :  mar. 20 oct. 2020 à 12:16
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `forum`
 --
-CREATE DATABASE IF NOT EXISTS `forum` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `forum`;
 
 -- --------------------------------------------------------
 
@@ -42,9 +40,9 @@ CREATE TABLE IF NOT EXISTS `categorie` (
 --
 
 INSERT INTO `categorie` (`id`, `titre`) VALUES
-(1, 'Assistance'),
-(2, 'Nouvelles et informations'),
-(3, 'Discussions sur le jeu');
+(1, 'aides'),
+(2, 'tout ce qui se passe'),
+(3, 'informations');
 
 -- --------------------------------------------------------
 
@@ -85,23 +83,27 @@ CREATE TABLE IF NOT EXISTS `forum_reponses` (
   `date_reponse` datetime NOT NULL,
   `correspondance_sujet` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=255 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=271 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `forum_reponses`
 --
 
 INSERT INTO `forum_reponses` (`id`, `auteur`, `message`, `date_reponse`, `correspondance_sujet`) VALUES
-(246, 'admin', 'test', '2020-01-07 10:53:51', 90),
-(239, 'admin', 'ok', '2020-01-05 12:23:19', 82),
-(200, 'admin', 'ok', '2019-12-28 15:17:26', 89),
-(197, 'admin', 'ok', '2019-12-24 15:10:01', 88),
-(194, 'admin', 'ok', '2019-12-24 11:52:31', 82),
-(247, 'admin', 'ok', '2020-01-07 10:54:54', 91),
-(248, 'admin', 'Salut tous !', '2020-01-07 17:42:06', 92),
-(249, 'admin', 'salut', '2020-01-09 10:21:43', 90),
-(253, 'Firefou', 'Posez vos questions', '2020-01-31 11:07:47', 93),
-(254, 'Walken99', 'Salut', '2020-02-11 14:34:07', 90);
+(246, 'admin', 'test', '2020-09-07 10:53:51', 90),
+(256, 'mouss', 'aller -y sur youtube, il y a des tutos pour debloquer certaines situation ', '2020-09-07 14:05:51', 95),
+(255, 'mouss', 'le niveau 132 bug est-ce normal ???', '2020-09-07 14:05:06', 94),
+(257, 'mouss', 'il y\'a la nouvelle version qui va sortir mais sur ps5', '2020-09-07 14:06:56', 96),
+(258, 'admin', 'ici vous pouvez débattre sur ce que vous voulez', '2020-09-07 14:08:06', 97),
+(259, 'mouss', 'mdr ok bien vu ;)', '2020-09-07 14:08:34', 97),
+(260, 'mouss', 'dfdsfdsdfdsfd', '2020-09-07 14:09:17', 98),
+(265, 'mouss', ':p', '2020-10-02 16:18:14', 100),
+(264, 'mouss', 'je suis bloqué dans un niveau', '2020-10-02 12:11:33', 99),
+(266, 'mouss', 'sur la psp il etait mieux', '2020-10-02 16:18:33', 92),
+(267, 'admin', 'Si vous galéré, un conseil allez-y sur youtube', '2020-10-02 16:29:38', 101),
+(268, 'admin', 'ici venez proposer vos solutions', '2020-10-02 16:29:59', 102),
+(269, 'admin', 'venez tous parler ici !!!!', '2020-10-02 16:31:00', 103),
+(270, 'admin', 'foire aux questions', '2020-10-02 16:32:02', 104);
 
 -- --------------------------------------------------------
 
@@ -118,17 +120,24 @@ CREATE TABLE IF NOT EXISTS `forum_sujets` (
   `correspondance_categorie` int(11) NOT NULL,
   `topic` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=94 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=105 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `forum_sujets`
 --
 
 INSERT INTO `forum_sujets` (`id`, `auteur`, `date_derniere_reponse`, `titre`, `correspondance_categorie`, `topic`) VALUES
-(90, 'admin', '2020-02-11 14:34:07', 'test', 1, 1),
-(92, 'admin', '2020-01-07 17:42:06', 'Gw2', 1, 3),
-(93, 'Firefou', '2020-01-31 11:07:47', 'FAQ', 1, 3),
-(89, 'admin', '2019-12-28 15:17:32', 'ok', 3, 14);
+(94, 'mouss', '2020-09-07 14:05:06', 'bug', 1, 1),
+(92, 'admin', '2020-10-02 16:18:33', 'god of war', 1, 3),
+(104, 'admin', '2020-10-02 16:32:02', 'FAQ', 1, 3),
+(95, 'mouss', '2020-09-07 14:05:51', 'triche', 1, 2),
+(96, 'mouss', '2020-09-07 14:06:56', 'nouveaux version', 2, 4),
+(97, 'admin', '2020-09-07 14:14:09', 'discussion', 3, 14),
+(99, 'mouss', '2020-10-02 12:11:33', 'help', 1, 1),
+(100, 'mouss', '2020-10-02 16:18:14', 'donnez vos avis', 2, 5),
+(101, 'admin', '2020-10-02 16:29:38', 'youtube', 1, 2),
+(102, 'admin', '2020-10-02 16:29:59', 'solution', 1, 1),
+(103, 'admin', '2020-10-02 16:31:00', 'chat entre joueurs', 3, 14);
 
 -- --------------------------------------------------------
 
@@ -142,7 +151,7 @@ CREATE TABLE IF NOT EXISTS `likes` (
   `id_forum_reponses` int(11) NOT NULL,
   `id_utilisateur` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=87 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=91 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `likes`
@@ -159,7 +168,11 @@ INSERT INTO `likes` (`id`, `id_forum_reponses`, `id_utilisateur`) VALUES
 (69, 197, 1),
 (63, 244, 1),
 (61, 242, 1),
-(51, 194, 4);
+(51, 194, 4),
+(87, 256, 8),
+(88, 257, 8),
+(89, 258, 1),
+(90, 260, 8);
 
 -- --------------------------------------------------------
 
@@ -180,14 +193,12 @@ CREATE TABLE IF NOT EXISTS `sous_categorie` (
 --
 
 INSERT INTO `sous_categorie` (`id`, `titre`, `id_categorie`) VALUES
-(1, 'Problème de compte et assistance technique', 1),
+(1, 'assistance technique', 1),
 (2, 'Bugs : jeu, forum et site web', 1),
 (3, 'Localisation / Traduction', 1),
 (4, 'Acutalités et annonces', 2),
 (5, 'Notes de mise à jour', 2),
-(14, 'Coin de la communauté', 3),
-(8, 'McM', 3),
-(9, 'JcJ', 3);
+(14, 'Coin de la communauté', 3);
 
 -- --------------------------------------------------------
 
@@ -202,7 +213,7 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `password` varchar(255) NOT NULL,
   `grade` int(11) NOT NULL DEFAULT '3',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `utilisateurs`
@@ -210,8 +221,8 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
 
 INSERT INTO `utilisateurs` (`id`, `login`, `password`, `grade`) VALUES
 (1, 'admin', '$2y$12$6DdTVMjrB9t6pcUoO2Gxwev2tSZFtQJVWCb9/CnhWkd3eHn3ntA4u', 1),
-(4, 'Firefou', '$2y$12$PjEtar2ZAUZIB2AbG6RNaObl9NIfQZVpM8anYKvc7Tl275EW.6Ntm', 3),
-(5, 'Walken99', '$2y$12$My8CCCtdtr/RzgSYHcRQqeMkaMqDwkx4KAQBhHsbG98bcMv0v/fcm', 3);
+(4, 'non', '$2y$12$PjEtar2ZAUZIB2AbG6RNaObl9NIfQZVpM8anYKvc7Tl275EW.6Ntm', 3),
+(8, 'mouss', '$2y$12$U99NysNVgYwsdgN3tEQaheZ0.POSAG.n9UFuHS/NrkXBHf9.egpyC', 3);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
